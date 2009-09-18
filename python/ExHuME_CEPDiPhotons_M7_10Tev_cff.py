84d99b59d17d367e4fce99a4703428f404db2176
Benoit Roland <sha1-2200346e613b7795d3e973c9eb4d9c0d63733b86@cern.ch>
2009-09-18 13:36:45 +0000

    Summer09 FAMC 10 TeV
    Former-commit-id: c35b0ddb38be8f5ec93a271b932a49a221934044

--- python/ExHuME_CEPDiPhotons_M7_10Tev_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-generator = cms.EDProducer("LHEProducer",
-        hadronisation = cms.PSet(
-                generator = cms.string('None')
-        )
-)
-
-
